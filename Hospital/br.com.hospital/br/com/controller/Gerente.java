58a7f44871bd6cc46e4a35890aa01e201626b23b
02977402101 <02977402101@192.168.56.1>
2019-08-24 02:43:30 -0400

    ATT

---
+++ Hospital/br.com.hospital/br/com/controller/Gerente.java
@@ -0,0 +1,21 @@
+package br.com.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+
+public class Gerente {
+
+    @FXML
+    private Button btnEnviar;
+
+    @FXML
+    private ChoiceBox<?> listCargo;
+
+    @FXML
+    void enviar(ActionEvent event) {
+
+    }
+
+}
--- Hospital/br.com.hospital/br/com/controller/Pessoa.java
+++ Hospital/br.com.hospital/br/com/controller/Pessoa.java
@@ -1,10 +1,10 @@
 package br.com.controller;
 
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.SplitMenuButton;
 import javafx.scene.control.TextField;
@@ -12,13 +12,7 @@ import javafx.scene.control.TextField;
 public class Pessoa {
 
     @FXML
-    private TextField txtLogin;
-
-    @FXML
     private Button btnEnviar;
-
-    @FXML
-    private PasswordField txtSenha;
 
     @FXML
     private RadioButton chkSexoF;
