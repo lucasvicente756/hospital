a48d5fac31a8b32fe0b7f63fd590059092fb3dbf
aluno <aluno@192.168.56.1>
2019-09-21 02:18:27 -0400

    att

--- Hospital/src/application/Main.java
+++ Hospital/src/application/Main.java
@@ -1,9 +1,12 @@
 package application;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import javax.swing.plaf.RootPaneUI;
-
+import br.com.hospital.conexao.Conexao;
 import br.com.hospital.util.Rotas;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -36,8 +39,26 @@ public class Main extends Application {
 			stageAtual.show();
 	}
 	
-	public static void main(String[] args) {
-		launch(args);
+	public Main() {
+		
+	}
+	
+	public static void main(String[] args) throws SQLException {
+		//launch(args);
+		
+		Conexao conn = new Conexao();
+		Connection conexao = conn.getConnection();
+		
+		System.out.println(conn.getStatus());
+		
+		String sqlINSERE = "INSERT INTO medico VALUES(?,?,?)";
+		
+		PreparedStatement stmt = conexao.prepareStatement(sqlINSERE);
+		stmt.setString(1,"1234-8");
+		stmt.setString(2,"Ofto");
+		stmt.setInt(3,'4');
+		stmt.execute();
+		
 	}
 
 }
