c0423cb8de28ad24410e7abb665dc18cde5251ab
PC <PC@PC-PC>
2019-10-17 15:19:21 -0300

    att

--- Hospital/src/br/com/hospital/DAO/EnfermidadeDAO.java
+++ Hospital/src/br/com/hospital/DAO/EnfermidadeDAO.java
@@ -22,46 +22,4 @@ public class EnfermidadeDAO {
 			stmt.setString(3,e.getDescricao());
 			stmt.execute();
 	}
-	public void  removeById(int id) throws SQLException {
-		Conexao conn = new Conexao();
-		Connection conexao = conn.getConnection();
-		System.out.println(conn.getStatus());
-		String sqlInsere = "DELETE  FROM Enfermidade WHERE id=(?)";	
-		PreparedStatement stmt = conexao.prepareStatement(sqlInsere);
-		stmt.setInt(1,id);
-		stmt.execute();
-
-	}
-
-	public void select(Enfermidade e) throws SQLException {
-		Conexao conn = new Conexao();
-		Connection conexao = conn.getConnection();
-		System.out.println(conn.getStatus());
-		String sqlInsere = "SELECT *  FROM Enfermidade";
-		PreparedStatement stmt = conexao.prepareStatement(sqlInsere);
-		ResultSet rs = stmt.executeQuery();
-		List<Enfermidade> enfermidades = new ArrayList<Enfermidade>();
-		while(rs.next()) {
-			Enfermidade e1 = new Enfermidade();
-			e1.setNome(rs.getString("nome"));
-			e1.setTipo(rs.getString("tipo"));
-			e1.setDescricao(rs.getString("tipo"));
-			enfermidades.add(e1);
-			
-		}
-			
-	}
-	public void updade(Enfermidade e) throws SQLException {
-		Conexao conn = new Conexao();
-		Connection conexao = conn.getConnection();
-		System.out.println(conn.getStatus());
-		String sqlInsere = "UPDATE Enfermidade SET(?,?,?) where id =(?)";
-		PreparedStatement stmt = conexao.prepareStatement(sqlInsere);
-			stmt.setInt(1,e.getIdEnfermidade());
-			stmt.setString(1,e.getNome());
-			stmt.setString(2,e.getTipo());
-			stmt.setString(3,e.getDescricao());
-			stmt.execute();
-	}
-	
 }
