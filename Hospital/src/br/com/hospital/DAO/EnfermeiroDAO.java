7ccdbfbb60b9f435c68738097c7950cf245e89a6
PC <PC@PC-PC>
2019-10-18 14:41:13 -0300

    att

--- Hospital/src/br/com/hospital/DAO/EnfermeiroDAO.java
+++ Hospital/src/br/com/hospital/DAO/EnfermeiroDAO.java
@@ -1,5 +1,29 @@
 package br.com.hospital.DAO;
 
-public class EnfermeiroDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import br.com.hospital.conexao.Conexao;
+import br.com.hospital.model.Enfermeiro;
 
+public class EnfermeiroDAO {
+	public void save(Enfermeiro e) throws SQLException  {
+		Conexao conn = new  Conexao();
+		Connection conexao =  conn.getConnection();
+		System.out.println(conn.getStatus());
+		String sqlInsere = "INSERT INTO Enfermeiro VALUES(?,?,?,?,?,?,?,?,?,?,?)";
+		PreparedStatement stmt = conexao.prepareStatement(sqlInsere);
+		stmt.setInt(1,e.getIdFuncionario());
+		stmt.setString(2,e.getNome());
+		stmt.setString(3, e.getCpf());
+		stmt.setString(4, e.getIdade());
+		stmt.setString(5, e.getTipoSanguineo());
+		stmt.setString(6, e.getSexo());
+		stmt.setString(7, e.getStatusPessoa());
+		stmt.setString(8, e.getLogin());
+		stmt.setString(9, e.getSenha());
+		stmt.setString(10, e.getStatusUsuario());
+		stmt.setInt(11, e.getNumeroderegistro());
+		stmt.execute();
+	}
 }
--- Hospital/src/br/com/hospital/controller/EnfermeiroControl.java
+++ Hospital/src/br/com/hospital/controller/EnfermeiroControl.java
@@ -1,8 +1,13 @@
 package br.com.hospital.controller;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 import application.Main;
+import br.com.hospital.DAO.EnfermeiroDAO;
+import br.com.hospital.DAO.MedicoDAO;
+import br.com.hospital.model.Enfermeiro;
+import br.com.hospital.model.Medico;
 import br.com.hospital.util.Rotas;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -48,8 +53,21 @@ public class EnfermeiroControl extends Main{
     private TextField numRegistro;
 
     @FXML
-    void SalvarEnfermeiro(ActionEvent event) {
-
+    void SalvarEnfermeiro(ActionEvent event) throws SQLException, IOException {
+    	Enfermeiro e = new Enfermeiro(); 
+    	EnfermeiroDAO  eDAO = new EnfermeiroDAO();
+		e.setNome(txtNomeEnfermeiro.getText());
+    	e.setCpf(numCpf.getText());
+    	e.setIdade(numIdade.getText());
+    	e.setTipoSanguineo(txtTipoSanguineo.getText());
+    	e.setSexo(txtSexo.getText());
+    	e.setStatusPessoa(statusEnfermeiro.getText());
+    	e.setLogin(txtLogin.getText());
+    	e.setSenha(txtSenha.getText());
+    	e.setStatusUsuario(statusCivil.getText());
+		e.setNumeroderegistro(Integer.parseInt(numRegistro.getText()));
+		eDAO.save(e);
+        openpage(Rotas.DASH);
     }
 
     @FXML
